ff688a1edbe82cabce4f44c4278b6e4264a8028c
SarojiniMaddaraki <152140351+SarojiniMaddaraki@users.noreply.github.com>
2026-02-17 22:20:05 +0530

    Add files via upload

---
+++ OPSMIND AI/backend/test.js
@@ -0,0 +1,18 @@
+const { Ollama } = require('ollama')
+
+const ollama = new Ollama({ host: 'http://127.0.0.1:11434' })
+
+async function run() {
+  try {
+    const response = await ollama.embed({
+      model: 'nomic-embed-text',
+      input: 'Hello world'
+    })
+
+    console.log(response.embeddings[0])
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+run()
