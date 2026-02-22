769fa4cc80c2b3feff70e606e91bd0e2a2abe2ad
SarojiniMaddaraki <152140351+SarojiniMaddaraki@users.noreply.github.com>
2026-02-22 22:42:56 +0530

    Delete OPSMIND AI/backend/models/documentModel.js

--- OPSMIND AI/backend/models/documentModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const documentSchema = new mongoose.Schema({
-  text: String,
-  embedding: [Number]
-})
-
-module.exports = mongoose.model('Document', documentSchema)
